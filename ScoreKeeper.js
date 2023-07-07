aa40fcb1f429f391ffa2e8a2233742922a46b70d
Abhigyan Singh <abhi02gyan@gmail.com>
2023-07-08 00:49:33 +0530

    Add files via upload

---
+++ ScoreKeeper.js
@@ -0,0 +1,55 @@
+const p1={
+    score: 0,
+    button: document.querySelector('#p1button'),
+    display: document.querySelector('#p1display')
+}
+
+const p2 = {
+    score: 0,
+    button: document.querySelector('#p2button'),
+    display: document.querySelector('#p2display')
+}
+
+const resetButton = document.querySelector('#reset');
+const winningScoreSelect = document.querySelector('#playto')
+
+let winningScore = 3;
+let isGameOver = false; 
+
+function updateScore(player,opponent) {
+    if (!isGameOver) {
+        player.score += 1;
+        if (player.score === winningScore) {
+            isGameOver = true;
+            player.display.classList.add('has-text-success');
+            opponent.display.classList.add('has-text-danger');
+            player.button.disabled = true;
+            opponent.button.disabled = true;
+        }
+        player.display.innerText = player.score;
+    }
+}
+
+p1.button.addEventListener('click', function () {
+    updateScore(p1, p2);
+})
+
+p2.button.addEventListener('click', function () {
+    updateScore(p2, p1);
+})
+
+winningScoreSelect.addEventListener('change', function () {
+    winningScore = parseInt(this.value);
+    reset();
+})
+
+resetButton.addEventListener('click', reset)
+function reset() {
+    isGameOver = false;
+    for (p of [p1, p2]) {
+        p.score = 0;
+        p.display.innerText = p.score;
+        p.display.classList.remove('has-text-success', 'has-text-danger')
+        p.button.disabled = false;
+    }
+}
